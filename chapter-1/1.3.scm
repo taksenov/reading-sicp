9c1cb854d7bc17e918402c40561d7b9d8e99b97d
TAksenov <taksenov@gmail.com>
2020-08-30 21:43:56 +0500

    Chapter 1: refactor: DRY for exercise 1.3

--- chapter-1/1.3.scm
+++ chapter-1/1.3.scm
@@ -3,15 +3,12 @@
 (define three 3)
 
 (define (sqr a) (* a a))
-(define (max-sqr-xy x y) 
+(define (sqr-max-of-two x y) 
         (cond ((> x y) (sqr x))
               (else (sqr y))))
-(define (max-sqr-yz y z) 
-        (cond ((> y z) (sqr y)) 
-              (else (sqr z))))
 
 (define (some-epic x y z)
-        (+ (max-sqr-xy x y) (max-sqr-yz y z))
+        (+ (sqr-max-of-two x y) (sqr-max-of-two y z))
 )
 
 (some-epic one two three)
